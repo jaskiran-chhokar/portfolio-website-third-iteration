08c265c7b2c680481c1a9c9e7b04c5beb85a09a9
Jaskiran <jaskiranchhokar@gmail.com>
2020-06-04 18:00:14 -0400

    Create function for header animation

--- public/js/animations.js
+++ public/js/animations.js
@@ -1,94 +1,99 @@
-let headerAnim = gsap.timeline();
+const headerAnimate = () => {
+    let headerAnim = gsap.timeline();
+    headerAnim.fromTo('.main-header', {
+        width: 0
+    },{
+        width:'100%', 
+        duration: 1
+    })
+    .fromTo('.navbar', {
+        width: 0
+    }, {
+        width:'100%', 
+        duration: 1
+    }, "-=1")
+    .to('.main-header', {
+        height: '82vh',
+        duration: 1
+    }, "-=1")
+    .fromTo('h1', {
+        y:20, 
+        opacity: 0, 
+        ease: Sine.easeOut
+    },{
+        duration: 0.5,
+        y:0,
+        opacity: 1,
+        ease: Sine.easeIn
+    },"-=0.1")
+    .fromTo('h2', {
+        y:20,
+        opacity: 0,
+        ease: Sine.easeOut
+    }, {
+        duration: 0.5,
+        y:0,
+        delay: 1.2,
+        opacity: 1,
+        ease: Sine.easeIn    
+    },"0")
+    .fromTo('.main-subheading', {
+        y:20,
+        opacity: 0,
+        ease: Sine.easeOut
+    }, {
+        duration: 0.5, 
+        y:0, 
+        delay: 1.4, 
+        opacity: 1,  
+        ease: Sine.easeIn
+    }, "0")
+    .fromTo('.main-button', {
+        y:20,
+        opacity: 0,
+        ease: Sine.easeOut
+    }, {
+        duration: 0.5,
+        y:0,
+        delay: 1.6,
+        opacity: 1, 
+        ease: Sine.easeIn
+    }, "0")
+    .fromTo('.fab', {
+        opacity: 0   
+    }, {
+        opacity: 1,
+        duration: 0.7,
+        stagger: .2,
+        delay: 1.8 
+    },"0")
+    .fromTo('.main--verticle-line', {
+        opacity: 0    
+    },{
+        opacity: 1,
+        duration: 0.7,
+        delay: 2.2
+    },"0")
+    .fromTo('.logo', {
+        ease: Power1.easeIn, 
+        x: -150
+    },{
+        x: 0,
+        duration: 0.5,
+        delay: 2.4
+    },"0")
+    .fromTo('.navbar__line', {
+        ease: Power1.easeIn,
+        x: 150,
+        opacity: 0   
+    },{
+        x: 0,
+        duration: 0.5,
+        delay: 2.4,
+        opacity: 1
+    },"0");    
+}; 
 
-headerAnim.fromTo('.main-header', {
-    width: 0
-},{
-    width:'100%', 
-    duration: 1
-})
-.fromTo('.navbar', {
-    width: 0
-}, {
-    width:'100%', 
-    duration: 1
-}, "-=1")
-.to('.main-header', {
-    height: '82vh',
-    duration: 1
-}, "-=1")
-.fromTo('h1', {
-    y:20, 
-    opacity: 0, 
-    ease: Sine.easeOut
-},{
-    duration: 0.5,
-    y:0,
-    opacity: 1,
-    ease: Sine.easeIn
-},"-=0.1")
-.fromTo('h2', {
-    y:20,
-    opacity: 0,
-    ease: Sine.easeOut
-}, {
-    duration: 0.5,
-    y:0,
-    delay: 1.2,
-    opacity: 1,
-    ease: Sine.easeIn    
-},"0")
-.fromTo('.main-subheading', {
-    y:20,
-    opacity: 0,
-    ease: Sine.easeOut
-}, {
-    duration: 0.5, 
-    y:0, 
-    delay: 1.4, 
-    opacity: 1,  
-    ease: Sine.easeIn
-}, "0")
-.fromTo('.main-button', {
-    y:20,
-    opacity: 0,
-    ease: Sine.easeOut
-}, {
-    duration: 0.5,
-    y:0,
-    delay: 1.6,
-    opacity: 1, 
-    ease: Sine.easeIn
-}, "0")
-.fromTo('.fab', {
-    opacity: 0   
-}, {
-    opacity: 1,
-    duration: 0.7,
-    stagger: .2,
-    delay: 1.8 
-},"0")
-.fromTo('.main--verticle-line', {
-    opacity: 0    
-},{
-    opacity: 1,
-    duration: 0.7,
-    delay: 2.2
-},"0")
-.fromTo('.logo', {
-    ease: Power1.easeIn, 
-    x: -150
-},{
-    x: 0,
-    duration: 0.5,
-    delay: 2.4
-},"0")
-.fromTo('.navbar__line', {
-    ease: Power1.easeIn,
-    x: 150,
-    opacity: 0   
-},{
-    x: 0,
-    duration: 0.5,
-    delay: 2.4,
-    opacity: 1
-},"0");
+headerAnimate();
+
+
