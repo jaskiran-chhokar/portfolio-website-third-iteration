978f7c3e60ac7507789e62f5a30e653ad8ff324b
Jaskiran <jaskiranchhokar@gmail.com>
2020-07-07 17:38:34 -0400

    Update navbar.js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,13 +1,31 @@
-const navbar = document.querySelector('.navbar__line');
-const navbarMobile = document.querySelector('.navbar__mobile');
+//Toggle Mobile Menu on click
+function mobileToggle() {
+    const navbar = document.querySelector('.navbar__line');
+    const navbarMobile = document.querySelector('.navbar__mobile');
 
+    navbar.addEventListener('click', e => {
+        e.target.classList.toggle('display-menu');
+        if (e.target.classList.contains('display-menu')) {
+            navbarMobile.style.display = 'block';
+        } else {
+            navbarMobile.style.display = 'none';
+        }
+    });
 
-navbar.addEventListener('click', e => {
-    e.target.classList.toggle('toggle-this');
+    closeMobileMenu(navbarMobile);
+}
 
-    if (e.target.classList.contains('toggle-this')) {
-        navbarMobile.style.display = 'block';
-    } else {
-        navbarMobile.style.display = 'none';
-    }
-}); 
+//Close Mobile Menu after Successful User Navigation
+function closeMobileMenu(navbarMobile) {
+    const nabvarMobileItems = document.querySelector('.navbar__items--mobile');
+    const navbarLine = document.querySelector('.navbar--line__two');
+
+    nabvarMobileItems.addEventListener('click', e => {
+        if (e.target.classList.contains('navbar__link--mobile')) {
+            navbarMobile.style.display = 'none';
+            navbarLine.classList.remove('display-menu')
+        }
+    });
+}
+
+mobileToggle();
