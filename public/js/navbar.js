1fd1aee2e8144f488ca2a9ab22f7f8781b3dd4aa
Jaskiran <jaskiranchhokar@gmail.com>
2020-07-07 02:05:15 -0400

    Add functionality for navbar in mobile

---
+++ public/js/navbar.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector('.navbar__line');
+const navbarMobile = document.querySelector('.navbar__mobile');
+
+
+navbar.addEventListener('click', e => {
+    e.target.classList.toggle('toggle-this');
+
+    if (e.target.classList.contains('toggle-this')) {
+        navbarMobile.style.display = 'block';
+    } else {
+        navbarMobile.style.display = 'none';
+    }
+
+
+    console.log("logged");
+}); 
